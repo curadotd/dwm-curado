d852bb4010ceea0eef9b756fa12188eb4abf428e
Marco Curado <home_info@curadotd.com>
2024-08-24 19:48:11 +0100

    add onboard to config for table suppor, testing

--- config.h
+++ config.h
@@ -13,6 +13,7 @@ static const int systraypinningfailfirst  = 1;      /* 1: if pinning fails, disp
 static const int showsystray              = 1;      /* 0 means no systray */
 static const int showbar                  = 1;      /* 0 means no bar */
 static const int topbar                   = 1;      /* 0 means bottom bar */
+static const char buttonbar[]       = "<O>";    /* defines the button bar text */
 #define ICONSIZE                            17      /* icon size */
 #define ICONSPACING                         5       /* space between icon and title */
 #define SHOWWINICON                         1       /* 0 means no winicon */
@@ -59,6 +60,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "onboard", NULL,	   NULL,		   0,		  True,		  False,	   -1,	False,	0.00,	1.0,	True,	True},
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
@@ -151,6 +153,7 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
+	{ ClkButton,            0,              Button1,        spawn,          {.v = onboard } },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkClientWin,         MODKEY,         Button1,        moveorplace,    {.i = 2} },
